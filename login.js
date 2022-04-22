c39175dda29426f7ac93f8ead89d0f6718a1c508
tschaefli <103317250+tschaefli@users.noreply.github.com>
2022-04-23 00:02:00 +0200

    mobile

--- login.js
+++ login.js
@@ -1,5 +1,9 @@
 function validate() {
-    var username = document.getElementById("username").Value;
-    var username = document.getElementById("password").Value;
-    alert("sucessfull");
+    var username = document.getElementById("username").value;
+    var password = document.getElementById("password").value;
+    if(username=="admin123" && password=="root") {
+        alert("done");
+    } else {
+        alert("failed");
+    }
 }
