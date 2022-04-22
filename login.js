bd699ea72917388f0c8bd4ddb29d827273188845
tschaefli <103317250+tschaefli@users.noreply.github.com>
2022-04-23 00:35:59 +0200

    fürabe

--- login.js
+++ login.js
@@ -1,9 +1,15 @@
 function validate() {
     var username = document.getElementById("username").value;
     var password = document.getElementById("password").value;
-    if(username=="admin123" && password=="root") {
-        alert("done");
+    if(username=="admin123") {
+        wronguser = false;
+        if(password=="root") {
+            wrongpw = false;
+            alert("sucessfull");
+        } else {
+            wrongpw = true;
+        }
     } else {
-        alert("failed");
+        wronguser = true;
     }
 }
